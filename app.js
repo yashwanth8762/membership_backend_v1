cb6429c2ec6c8ad8f4fe6594f9aad1dc38d2c393
gcnaveen <naveengc99@gmail.com>
2025-09-22 11:09:06 +0530

    payment

--- app.js
+++ app.js
@@ -40,8 +40,9 @@ app.use(cors());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 app.use(morgan("dev"));
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '2048mb' }));
+app.use(express.urlencoded({ extended: true, limit: '2048mb' }));
+
 
 const API_ROOT ='/api/' 
 app.use(`${API_ROOT}assets`, express.static(path.join(__dirname, "assets")));
