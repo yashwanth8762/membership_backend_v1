9425118fe7dadb7a824326c6220a3bbf9be63285
gcnaveen <naveengc99@gmail.com>
2026-02-15 10:51:56 +0530

    added

--- importData.js
+++ importData.js
@@ -1328,7 +1328,7 @@ async function importData(filePath, defaultFormId = null) {
 // Main function
 (async () => {
   await connectDB();
-  const filePath = './bengaluru999.xlsx';
+  const filePath = './bengaluru9991.xlsx';
   
   try {
     await importData(filePath);
